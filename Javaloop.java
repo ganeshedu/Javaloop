a69b1f0170980d4bd16a53f4f63bc6a767404087
ganesh manjuluri <ganesh.edu@gmail.com>
2018-10-20 17:38:18 -0400

    This is a to load code to Git and commit

---
+++ Javaloop.java
@@ -0,0 +1,12 @@
+/* JavaLoop.java 
+	Sample Java Program */
+
+public class Javaloop {
+
+	public static void main(String[] args) {
+
+	for(int i=1;i<=10;i++){
+	      System.out.println("The number is: " + i);
+                         }		
+		}
+}
