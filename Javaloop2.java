18a86dab2e8d90cadac7e2e133b66a24c0fd45ea
ganesh manjuluri <ganesh.edu@gmail.com>
2018-10-21 07:47:32 -0400

    This is a to add Javaloop 2 & 3 code to be  added

---
+++ Javaloop2.java
@@ -0,0 +1,12 @@
+/* JavaLoop2.java 
+	Sample Java Program -2  */
+
+public class Javaloop2 {
+
+	public static void main(String[] args) {
+
+	for(int i=10;i<=20;i++){
+	      System.out.println("The number is: " + i);
+                         }		
+		}
+}
---
+++ Javaloop3.java
@@ -0,0 +1,12 @@
+/* JavaLoop3.java 
+	Sample Java Program -3  */
+
+public class Javaloop3 {
+
+	public static void main(String[] args) {
+
+	for(int i=20;i<=30;i++){
+	      System.out.println("The number is: " + i);
+                         }		
+		}
+}
